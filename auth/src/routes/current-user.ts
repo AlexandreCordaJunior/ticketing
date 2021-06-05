b9a810454dbcbf509f8ee47cdadbc287134163b7
Alexandre Corda <alexandre.corda@fatec.sp.gov.br>
2021-06-04 21:28:04 -0300

    auth service finalizado, iniciando ticketing service

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { currentUser } from '../middlewares/current-user';
+import { currentUser } from '@alexandre-corda/common';
 
 const router = express.Router();
 
