b63a984b5ac4d1fc22efb960aaeadc4b9e0ca5d4
Alexandre Corda <alexandre.corda@fatec.sp.gov.br>
2021-06-26 22:58:29 -0300

    tickets app

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -3,7 +3,9 @@ import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 
-import { errorHandler, NotFoundError } from '@alexandre-corda/common';
+import { currentUser, errorHandler, NotFoundError } from '@alexandre-corda/common';
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
 
 const app = express();
 app.set('trust proxy', true);
@@ -14,6 +16,11 @@ app.use(
     secure: process.env.NODE_ENV !== "test"
   })
 );
+
+app.use(currentUser);
+
+app.use(showTicketRouter);
+app.use(createTicketRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -1,12 +1,13 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
-import { app } from "../app";
-import request from "supertest";
+/* import { app } from "../app";
+import request from "supertest"; */
+import jwt from "jsonwebtoken";
 
 declare global {
     namespace NodeJS {
         interface Global {
-            signin(): Promise<string[]>
+            signin(): string[]
         }
     }
 }
@@ -26,7 +27,7 @@ beforeAll(async () => {
 
 beforeEach(async () => {
     const collections = await mongoose.connection.db.collections();
-    for(let collection of collections) {
+    for (let collection of collections) {
         await collection.deleteMany({});
     }
 });
@@ -36,17 +37,16 @@ afterAll(async () => {
     await mongoose.connection.close();
 });
 
-global.signin = async () => {
-    const email = "test@test.com";
-    const password = "password";
+global.signin = () => {
+    const payload = {
+        id: "abc",
+        email: "test@test.com"
+    };
 
-    const response = await request(app)
-        .post("/api/users/signup")
-        .send({
-            email, password
-        })
-        .expect(201);
+    const token = jwt.sign(payload, process.env.JWT_KEY!);
+    const session = { jwt: token };
+    const sessionJSON = JSON.stringify(session);
 
-    const cookie = response.get("Set-Cookie");
-    return cookie;
+    const base64 = Buffer.from(sessionJSON).toString("base64");
+    return [`express:sess=${base64}`];
 };
