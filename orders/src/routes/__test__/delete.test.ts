e16f4660e49ef9bcf092501c8a677017e292f96e
Alexandre Corda <alexandre.corda@fatec.sp.gov.br>
2021-09-14 22:28:32 -0300

    orders service

---
+++ orders/src/routes/__test__/delete.test.ts
@@ -0,0 +1,39 @@
+import request from 'supertest';
+import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
+import {Order} from "../../models/order";
+import {OrderStatus} from "@alexandre-corda/common";
+
+const buildTicket = async () => {
+    const ticket = Ticket.build({
+        title: 'concert',
+        price: 20
+    });
+
+    await ticket.save();
+    return ticket;
+};
+
+it('marks an order as cancelled', async () => {
+    const ticket = await buildTicket();
+
+    const user = global.signin();
+
+    const { body: order } =await request(app)
+        .post('/api/orders')
+        .set('Cookie', user)
+        .send({
+            ticketId: ticket.id
+        });
+
+    await request(app)
+        .delete(`/api/orders/${order.id}`)
+        .set('Cookie', user)
+        .send()
+        .expect(204);
+
+    const updatedOrder = await Order.findById(order.id);
+    expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
+});
+
+it.todo('emits an order cancelled event');
